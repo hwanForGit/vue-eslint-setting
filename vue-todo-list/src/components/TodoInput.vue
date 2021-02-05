a5bd58e4578b61d4c1b610ee3b9749349a478085
hwanforgit <hwanforgit@gmail.com>
2021-02-05 17:36:35 +0900

    210205
    1. Adding Components
    2. Registering TodoList
    3. Delete TodoList

---
+++ vue-todo-list/src/components/TodoInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input v-model="newTodoItem" type="text" />
+    <button @click="addTodo">추가</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: ''
+    };
+  },
+  methods: {
+    addTodo() {
+      // console.log(this.newTodoItem);
+      if (this.newTodoItem !== '') {
+        const value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInputBox();
+      }
+    },
+    clearInputBox() {
+      this.newTodoItem = '';
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ vue-todo-list/src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <!--<div>TodoList</div>-->
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem">
+        {{ todoItem }}
+        <button @click="removeTodo(todoItem, index)">삭제</button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    removeTodo(todoItem, index) {
+      // 삭제 기능
+      // console.log(todoItem, index);
+    }
+  }
+};
+</script>
+
+<style></style>
